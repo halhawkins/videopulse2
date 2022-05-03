8b0acc824da812d8188d7fdd5121da39370e85f9
Hal Hawkins <hal.hawkins@gmail.com>
2022-05-03 11:25:01 -0400

    media summaries missing overview.

--- src/PopularMovies/PopularMovies.js
+++ src/PopularMovies/PopularMovies.js
@@ -45,7 +45,7 @@ class PopularMovies extends Component{
                 <>
                     <div className="content-title"><h2 className="page-title">Popular Movies{this.search}</h2></div>
                     {items.map((item,i) => {
-                            return <MediaSummary size={"normal"} passThru={() => this.detailsPassThru("details",item.id,"movie")} key={"item"+item.id} mediaID={item.id} mediaType={"movie"} poster_path={item.poster_path} vote_average={item.vote_average} media_name={item.title}/>
+                            return <MediaSummary overview={item.overview} size={"normal"} passThru={() => this.detailsPassThru("details",item.id,"movie")} key={"item"+item.id} mediaID={item.id} mediaType={"movie"} poster_path={item.poster_path} vote_average={item.vote_average} media_name={item.title}/>
                         })
                     }
                     <PaginationControl contentPage="popularmovies" maxentries={10} pageNumberCallback={null} pageQueryVariable="page" q={null} currentPage={this.props.p} lastPage={this.state.totalPages} link={`?page=popularmovies&language=${this.lang}&p=`} />
